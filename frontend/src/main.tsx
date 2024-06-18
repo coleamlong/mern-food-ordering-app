140d9b394e123c80ed1168622e304e3e77c47b52
Cole Amlong <coleamlong@gmail.com>
2024-06-18 17:08:45 -0500

    Fixed naming

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import "./global.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import AppRoutes from "./AppRoutes";
+import AppRoutes from "./appRoutes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
